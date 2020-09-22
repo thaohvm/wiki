2245889c38d12f675bf408c330f9e1fca4f61642
thaohvm <43254779+thaohvm@users.noreply.github.com>
2020-09-22 00:15:06 -0700

    question 3 finished create new page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,11 +2,16 @@ from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
+
 class NewForm(forms.Form):
-    new = forms.CharField(label="New Title")
+    title = forms.CharField(label="Title")
+    content = forms.CharField(
+        label="Content", widget=forms.Textarea(attrs={'rows': 4, 'cols': 15}))
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -33,18 +38,12 @@ def search(request):
         return HttpResponseNotFound("Page not found")
 
 
-# def add(request):
-#     return render(request, "encyclopedia/add.html", {
-#         "add": add,
-#     })
-
-
 def add(request):
     if request.method == "POST":
         form = NewForm(request.POST)
         if form.is_valid():
-            new = form.cleaned_data["new"]
-            request.session["new"] += [new]
+            util.save_entry(
+                form.cleaned_data["title"], form.cleaned_data["content"])
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "encyclopedia/add.html", {
